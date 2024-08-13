539a34902a246b4bbe7b567b26a7e814dfe85740
SkyfallWasTaken <mahadkalam1@proton.me>
2024-08-13 16:52:31 +0100

    Add header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Button from './ui/button/button.svelte';
+	import Separator from './ui/separator/separator.svelte';
+	import ThemeToggle from './ThemeToggle.svelte';
+</script>
+
+<div
+	class="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16"
+>
+	<h2 class="w-full text-lg font-semibold">Skyfall Upload</h2>
+	<div class="ml-auto flex w-full space-x-2 sm:justify-end">
+		<ThemeToggle />
+		<Button>Sign in</Button>
+	</div>
+</div>
+<Separator />
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { ModeWatcher } from 'mode-watcher';
+
+	import Header from '$lib/components/Header.svelte';
+	import '../app.css';
+</script>
+
+<ModeWatcher />
+<Header />
+<slot></slot>
