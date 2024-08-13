cfa637dff762467780e47f50cf203bb9206a5a36
SkyfallWasTaken <mahadkalam1@proton.me>
2024-08-13 17:47:53 +0100

    WIP auth

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -3,8 +3,8 @@ import { dev } from '$app/environment';
 
 import pg from 'pg';
 import { DrizzlePostgreSQLAdapter } from '@lucia-auth/adapter-drizzle';
-import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 import { drizzle } from 'drizzle-orm/node-postgres';
+import { sessionTable, userTable } from './schema';
 
 import { GitHub } from 'arctic';
 import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
@@ -15,21 +15,6 @@ const pool = new pg.Pool({
 	connectionString: import.meta.env.PG_DATABASE_URL
 });
 export const db = drizzle(pool);
-
-export const userTable = pgTable('user', {
-	id: text('id').primaryKey()
-});
-
-const sessionTable = pgTable('session', {
-	id: text('id').primaryKey(),
-	userId: text('user_id')
-		.notNull()
-		.references(() => userTable.id),
-	expiresAt: timestamp('expires_at', {
-		withTimezone: true,
-		mode: 'date'
-	}).notNull()
-});
 
 const adapter = new DrizzlePostgreSQLAdapter(db, sessionTable, userTable);
 
