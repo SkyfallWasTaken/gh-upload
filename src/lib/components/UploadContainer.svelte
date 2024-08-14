0d08c0127c5fd0c15bd58064b29542c9efefce36
SkyfallWasTaken <mahadkalam1@proton.me>
2024-08-14 10:15:27 +0100

    WIP upload container

---
+++ src/lib/components/UploadContainer.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { Button } from './ui/button';
+	import Upload from 'lucide-svelte/icons/upload';
+	import LockKeyhole from 'lucide-svelte/icons/lock-keyhole';
+
+	export let loggedIn: boolean;
+</script>
+
+<div
+	class="container flex flex-col items-center gap-3 rounded-lg border-4 border-dashed border-slate-400 bg-secondary p-6"
+>
+	{#if loggedIn}
+		<h2 class="text-xl font-semibold">Select a file</h2>
+		<p class="text-muted-foreground">Or drag and drop to upload</p>
+		<Button>
+			<Upload class="mr-2 h-4 w-4" />
+			Choose a file
+		</Button>
+	{:else}
+		<h2 class="text-xl font-semibold">First, sign in to GitHub</h2>
+		<p class="text-muted-foreground">Once you're done, we'll go to the next stage.</p>
+		<Button>
+			<LockKeyhole class="mr-2 h-4 w-4" />
+			Sign in to GitHub
+		</Button>
+	{/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import UploadContainer from '$lib/components/UploadContainer.svelte';
 
 	export let data: PageData;
 </script>
 
-<h1>Welcome to SvelteKit, {data.loggedIn ? data.username : 'please sign in'}</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="px-2 py-4">
+	<div class="flex flex-col items-center justify-center gap-1">
+		<h1 class="my-4 text-center text-6xl font-bold">Uploads made<br />simple</h1>
+		<p>This is a simple file upload service.</p>
+		<p>Click the button below to upload a file.</p>
+		<UploadContainer loggedIn={data.loggedIn} />
+	</div>
+</main>
